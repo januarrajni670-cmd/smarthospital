0b1f39ff3d7a8660c89ba8776b2db38fc4c55123
januarrajni670-cmd <januarrajni670@gmail.com>
2026-07-04 17:01:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ import os
 
 # region 🎨 1. PENGATURAN TAMPILAN & CSS (ABAIKAN BAGIAN INI)
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pickle
-import os
-
-# region 🎨 1. PENGATURAN TAMPILAN & CSS (ABAIKAN BAGIAN INI)
-st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
