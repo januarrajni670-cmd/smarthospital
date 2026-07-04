6b9f8bb0d0f06eeb572be8cf13c4633ae506310d
januarrajni670-cmd <januarrajni670@gmail.com>
2026-07-04 16:48:34 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+# region 🎨 1. PENGATURAN TAMPILAN & CSS (ABAIKAN BAGIAN INI)
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
+
+
